8c768f59e2a0749fda7b4dff6f14aadf991305f5
dookim <doo871128@gmail.com>
2015-02-08 23:16:32 +0900

    inserting data into queue was complete

--- controllers/url_control.js
+++ controllers/url_control.js
@@ -20,9 +20,18 @@ var get_cur_time = function() {
     var curtime = sprintf('%04d-%02d-%02d %02d:%02d:%02d', year, month, day, hours, min, sec);
     return curtime;
 };
+//there is possibility if i delete method, this application would not operate properly
+exports.post_test = function(req, res) {
+   res.send(req.body);
 
-exports.connect = function(req, res) {
-    var data = { 'tag':'connect','data': req.body,'date_time': get_cur_time()};
+}
+
+exports.get_test = function(req,res) {
+   res.send("hello");
+}
+
+exports.login = function(req, res) {
+    var data = { 'tag':'login','data': req.body};
     
     //# set queue name for worker and queue
     /*
@@ -42,38 +51,14 @@ exports.connect = function(req, res) {
 }
 
 
-exports.receive_exception = function(req, res) {
-    //console.log("exception Data");
-    var _id = req.body.instance;
-    var version = req.body.version;
-    var _data  = {};
-
-    //# set queue name for worker and queue
-    
-    /*
-    var client_type = req.query.client_type;
-    var queueName = queueName;
-    if(client_type) queueName = client_type;
-    */
-
-
-    if ( version ){
-        _data.tag = 'receive_exception'
-        _data.data = req.body.exception;
-        _data.log = req.body.console_log.data;
-        _data.date_time = get_cur_time();
-    }
-    else{
-        _data.tag = 'receive_exception'
-        _data.data = req.body;
-        _data.date_time = get_cur_time();
-    }
-
-
-
+exports.register_book = function(req, res) {
+    console.log("register book");
+    var data = {'tag':'register_book','data':req.body};
+    console.log(data);
+    console.log(queueName);
     gk.async.sequence([
         function (cb) {
-            mq_pubhandler.publish(queueName, _data);
+            mq_pubhandler.publish(queueName, data);
             cb();
         }
     ], function (err) {
@@ -81,69 +66,3 @@ exports.receive_exception = function(req, res) {
         res.send(result);
     });
 }
-
-exports.receive_native = function(req, res) {
-
-    var data = { 'tag':'receive_native', 'data': req.body.exception ,'log':req.body.console_log.data,'dump_data':req.body.dump_data, 'date_time':get_cur_time()};
-
-    //# set queue name for worker and queue
-    /*
-    var client_type = req.query.client_type;
-    var queueName = queueName;
-    if(client_type) queueName = client_type;
-    */
-
-    //console.log("native dump data");
-
-    gk.async.sequence([
-        function (cb) {
-            mq_pubhandler.publish(queueName, data);
-            cb();
-        }
-    ], function (err) {
-        var result = { 'state': 'success'};
-        res.send(result);
-    });
-};
-
-exports.receive_test_data = function(req, res) {
-
-    var data = { 'tag':'receive_native_dump','data': req.body.img, 'date_time':get_cur_time()};
-    //console.log(data.date_time);
-    //console.log(req.body.img)
-
-    //# set queue name for worker and queue
-    /*
-    var client_type = req.query.client_type;
-    var queueName = queueName;
-    if(client_type) queueName = client_type;
-    */
-    gk.async.sequence([
-        function (cb) {
-            mq_pubhandler.publish(queueName, data);
-            cb();
-        }
-    ], function (err) {
-        var result = { 'state': 'success'};
-        res.send(result);
-    });
-
-};
-
-
-
-exports.url_redirect = function(req,res) {
-    // res.redirect('http://ur-qa.com:9000');
-    gk.async.sequence([
-        function (cb) {
-            res.writeHead(301,
-                {
-                    Location: 'http://urqa.io'
-                });
-            res.end();
-        }
-    ], function (err) {
-        res.send(result);
-    });
-};
-
--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,15 @@ var url_control_enc = require(__dirname + '/../controllers/url_control_enc');
 
 var routes = function(app) {
 	// this is for legact rest apis for android
-	app.post('/urqa/client/connect', url_control.connect);
-	app.post('/urqa/client/send/exception', url_control.receive_exception);
-	app.post('/urqa/client/send/exception/native', url_control.receive_native);
-	app.post('/test/',url_control.receive_test_data);
-	app.get('/$',url_control.url_redirect);
-	app.post('/urqa', url_control.url_redirect);
-
+	app.post('/zoco/client/login',url_control.login);
+	app.post('/zoco/client/register_book',url_control.register_book);
+	app.post('/zoco/client/post_test', url_control.post_test);
+	app.get('/zoco/client/get_test',url_control.get_test);
 	// encrypt
-	app.post('/urqa/client/get_key', url_control_enc.get_key );
-	app.post('/urqa/client/req_enc', url_control_enc.req_enc );
+	app.post('/zoco/client/get_key', url_control_enc.get_key );
 
 	// jsonp
-	app.get('/urqa/client/jsonp', url_control_jsonp.jsonp_wrapper );
+	app.get('/zoco/client/jsonp', url_control_jsonp.jsonp_wrapper );
 
 	// Encrypt Test
 	app.post('/test_enc', url_control_enc.url_test_enc );
